10fbb194b447b0edbed51ae01a882f417f7ed9fd
kevinwangfencing <55242780+kevinwangfencing@users.noreply.github.com>
2021-02-23 11:47:22 -0500

    Update chapter_06.py

--- labs/labs/chapter_06.py
+++ labs/labs/chapter_06.py
@@ -1,6 +1,6 @@
 # labs/chapter_06.py
 
-# 6.1 part 1
+# 6.1 part 1 =====================================================
 x = 10
 for row in range(1,10):
     for column in range(row):
@@ -12,7 +12,7 @@ for row in range(1,10):
     print()
     
     
-# 6.2 part 2
+# 6.2 part 2 =====================================================
 # Step 1: Claculate rows and columns
 n = 0
 while True:
@@ -47,7 +47,7 @@ for i in range(columns):
     print("o", end = "")
 
 
-# 6.3 part 3
+# 6.3 part 3 =====================================================
 # Get user input
 n = int(input("Enter number:"))
 
@@ -75,3 +75,70 @@ for row in reversed(range(n)):
         print(2*i + 1, end = " ")
 
     print()
+
+    
+# 6.4 part 4 =====================================================
+# Import a library of functions called 'pygame'
+import pygame
+ 
+# Initialize the game engine
+pygame.init()
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (102, 204, 0)
+RED = (255, 0, 0)
+DARK_RED = (154, 32, 32)
+LIGHT_YELLOW = (255, 255, 102)
+SKY_BLUE = (204,255,255)
+
+
+# Set the height and width of the screen
+size = (400, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("Kevin's picture")
+ 
+# Loop until user click quit
+done = False
+
+# Manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# Loop as long as done == False
+while done == False:
+ 
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+ 
+    # Clear the screen and set the screen background
+    screen.fill(WHITE)
+
+    # -------------Main drawings here---------
+    
+    # Draw picture
+    square = 0
+    rows = 400
+    columns = 500
+
+    height = (columns/25)-10
+    width = (rows/20)-10
+    for i in range (rows):
+        for j in range(columns):
+            pygame.draw.rect(screen,
+                            LIGHT_YELLOW,
+                            [0 + (j * (width + 5)), 0 + (i * (height + 5)),
+                            width, height])
+            
+
+    # Flip the Pygame display
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
