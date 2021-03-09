67636d0f6ab95bccf2bd77054840bce913eed9cf
kevinwangfencing <55242780+kevinwangfencing@users.noreply.github.com>
2021-03-09 13:22:43 -0500

    Update chapter_9.py

--- labs/labs/chapter_9.py
+++ labs/labs/chapter_9.py
@@ -31,3 +31,17 @@ print()   # Blank line
 box(3,2)  # Print a box 3 high, 2 across
 print()   # Blank line
 box(3,10) # Print a box 3 high, 10 across
+
+# ----------------------------------------------------
+my_list = [36, 31, 79, 96, 36, 91, 77, 33, 19, 3, 34, 12, 70, 12, 54, 98, 86, 11, 17, 17]
+
+def find(my_list,key):
+    index = 0
+    for i in range(len(my_list)):
+        if key == my_list[i]:
+            index = i
+            print(f"FOUND {key} at position {index}")
+
+find(my_list, 12)
+find(my_list, 91)
+find(my_list, 80)
